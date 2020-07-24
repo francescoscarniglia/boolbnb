04c07c0b0a084a2092701fb3c1b28adae1dac0df
Fabio Gadau <fabiogadau92@gmail.com>
2020-07-24 18:26:12 +0200

    styles login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -84,7 +84,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row ">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
